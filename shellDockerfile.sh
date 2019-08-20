48eea01108db21516d2a877e6f8265e2e835f3f6
alabrashjr <alabrash.abd@gmail.com>
2019-08-20 16:28:30 +0300

    update the shell docker file

--- shellDockerfile.sh
+++ shellDockerfile.sh
@@ -1,10 +1,14 @@
 apt --fix-broken install && apt-get -qq update && apt-get install -qq -y --no-install-recommends \
     ca-certificates \
+    curl \
     gawk \
     libexpat1 \
     libpq5 \
     mysql-client \
     nginx \
+    python \
+    python-setuptools \
+    python-pip \
     python-crypto \
     python-flask \
     python-pil \
@@ -14,7 +18,8 @@ apt --fix-broken install && apt-get -qq update && apt-get install -qq -y --no-in
     uwsgi-plugin-python \
 && pip install -q natsort
 
-wget http://repo.mysql.com/apt/debian/pool/mysql-5.6/m/mysql-community/libmysqlclient18_5.6.45-1debian9_amd64.deb && dpkg -i libmysqlclient18_5.6.45-1debian9_amd64.deb
+wget http://repo.mysql.com/apt/debian/pool/mysql-5.6/m/mysql-community/libmysqlclient18_5.6.45-1debian9_amd64.deb && dpkg -i libmysqlclient18_5.6.45-1debian9_amd64.deb && rm libmysqlclient18_5.6.45-1debian9_amd64.deb
+echo exit 0 > /usr/sbin/policy-rc.d
 
 curl -s \
     http://sphinxsearch.com/files/sphinxsearch_2.2.11-release-1~jessie_amd64.deb \
@@ -35,13 +40,15 @@ cp conf/nginx/nginx.conf /etc/nginx/sites-available/default
 cp supervisor/*.conf /etc/supervisor/conf.d/
 cp supervisord.conf /etc/supervisor/supervisord.conf
 cp -r web /usr/local/src/websearch
-#cp sphinx-reindex.sh /
-#cp -r  tests/* /tests/
+cp sphinx-reindex.sh /
+mkdir /tests
+cp -r  tests/* /tests/
 
 export SPHINX_PORT=9312 \
     SEARCH_MAX_COUNT=100 \
     SEARCH_DEFAULT_COUNT=20
 
+echo "FINSIH"
 #EXPOSE 80
 #to run 
 #/usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
